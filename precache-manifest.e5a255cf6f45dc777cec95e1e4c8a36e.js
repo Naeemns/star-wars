1f048452240052b15d22cba44ad39b09183b9465
Naeemns <naeemshaik894@gmail.com>
2021-05-30 01:18:04 +0530

    Updates

---
+++ precache-manifest.e5a255cf6f45dc777cec95e1e4c8a36e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cb030791eaa6a3afeda856d34a7c09",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "a45c830f1800ace3ca56",
+    "url": "/star-wars/static/css/main.613b8428.chunk.css"
+  },
+  {
+    "revision": "299ebd568a968feee85a",
+    "url": "/star-wars/static/js/2.99fcd1ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars/static/js/2.99fcd1ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45c830f1800ace3ca56",
+    "url": "/star-wars/static/js/main.671d3960.chunk.js"
+  },
+  {
+    "revision": "577fc8d045a6b4f41f3d",
+    "url": "/star-wars/static/js/runtime-main.5c19e104.js"
+  },
+  {
+    "revision": "0e1b90ab170e6895c9b57cde75da003d",
+    "url": "/star-wars/static/media/loading.0e1b90ab.gif"
+  }
+]);
